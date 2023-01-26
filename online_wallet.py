34ecae9e773573c687b40456bb6f0010ccfb5d43
EkaterinaDJ <dejongekaterina@gmail.com>
2023-01-26 21:14:41 +0300

    guest_list

--- online_wallet.py
+++ online_wallet.py
@@ -1,11 +1,38 @@
+# class Customers:
+#     def __init__(self, name, surname, city, balance):
+#         self.name = name
+#         self.surname = surname
+#         self.city = city
+#         self.balance = balance
+#     def __str__(self):
+#         return f'"{self.name} {self.surname}. {self.city}. Баланс: {self.balance} руб."'
+#
+# Customer_1 = Customers('Иван',  'Петров', 'Москва', 50)
+# print(Customer_1)
+
 class Customers:
-    def __init__(self, name, surname, city, balance):
+    def __init__(self, name, surname, city):
         self.name = name
         self.surname = surname
         self.city = city
-        self.balance = balance
-    def __str__(self):
-        return f'''"{self.name} {self.surname}. {self.city}. Баланс: {self.balance} руб."'''
 
-Customer_1 = Customers('Иван',  'Петров', 'Москва', 50)
-print(Customer_1)
+    # def __str__(self):
+    #     return f'"{self.name} {self.surname}. {self.city}."'
+
+    def get_guest(self):
+        return f'{self.name} {self.surname}. {self.city}.'
+
+
+Customer_1 = Customers('Иван',  'Петров', 'Москва')
+Customer_2 = Customers('Петр', 'Иванов', 'Казань')
+Customer_3 = Customers('Елена', 'Федорова', 'Псков')
+Customer_4 = Customers('Василий','Сергеев', 'Гатчина')
+Customer_5 = Customers('Анна', 'Сергеева', 'Гатчина')
+
+guest_list = [Customer_1, Customer_2, Customer_3, Customer_4, Customer_5]
+for guest in guest_list:
+    print(guest.get_guest())
+
+
+# for
+# print(Customer_1)
